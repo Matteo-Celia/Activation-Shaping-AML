c51eb381a5d9301dba3c1690cc61f9c4ae9ea36e
frogeye00 <CLEMTT00E08G371P@studium.unict.it>
2024-01-02 10:54:37 +0100

    Update baseline.sh

--- launch_scripts/baseline.sh
+++ launch_scripts/baseline.sh
@@ -6,5 +6,5 @@ python main.py \
 --experiment_name=${exp}/${target_domain}/ \
 --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}'}" \
 --batch_size=256 \
---num_workers=5 \
+--num_workers=2 \
 --grad_accum_steps=1
