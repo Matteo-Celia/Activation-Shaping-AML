c14fbc197c60fe1188f33a4d780ea95db432e3de
frogeye00 <CLEMTT00E08G371P@studium.unict.it>
2024-01-13 14:36:24 +0100

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -60,7 +60,7 @@ class ASHResNet18(nn.Module):
 
         if penultimate:
             # Access the penultimate layer (before GAP) in ResNet18
-            target_layer = self.resnet.layer4[0].bn1#list(self.resnet.modules())[-7]  # Access the specific layer
+            target_layer = self.resnet.avgpool #self.resnet.layer4[0].bn1#list(self.resnet.modules())[-7]  # Access the specific layer
 
             # Register forward hook on the penultimate layer
             hook=target_layer.register_forward_hook(activation_shaping_hook(M))
@@ -81,7 +81,7 @@ class ASHResNet18(nn.Module):
             activations.append( output.detach())
 
         if penultimate:
-            target_layer = self.resnet.layer4[0].bn1
+            target_layer = self.resnet.avgpool #self.resnet.layer4[0].bn1
             hook = target_layer.register_forward_hook(hook_fn)
             hooks.append(hook)
         else:
