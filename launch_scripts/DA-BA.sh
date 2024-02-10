0399824c3722fc63ce43080f3395834debb0a7a1
frogeye00 <CLEMTT00E08G371P@studium.unict.it>
2024-02-10 11:22:11 +0100

    Update DA-BA.sh

--- launch_scripts/DA-BA.sh
+++ launch_scripts/DA-BA.sh
@@ -3,8 +3,8 @@ target_domain=${2}
 
 
 python main.py \
---experiment= ${exp} \
---experiment_name= ${exp}/${target_domain}/ \
+--experiment=${exp} \
+--experiment_name=${exp}/${target_domain}/ \
 --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}' }" \
 --batch_size=128 \
 --num_workers=5 \
