79e19822731c5bd26d79bc174932fdae8abd31ab
frogeye00 <CLEMTT00E08G371P@studium.unict.it>
2024-02-11 13:06:11 +0100

    da

--- main.py
+++ main.py
@@ -72,8 +72,8 @@ def train(model: BaseResNet18, data):
             hook_handles = []
             #hook_handles = register_forward_hooks(model, model.rec_actmaps_hook, nn.Conv2d)
             #hook_handles += register_forward_hooks(model, model.asm_source_hook, nn.Conv2d)
-            hook_handles.append(model.resnet.layer4[1].bn1.register_forward_hook(model.rec_actmaps_hook))
-            hook_handles.append(model.resnet.layer4[1].bn1.register_forward_hook(model.asm_source_hook))
+            #hook_handles.append(model.resnet.layer4[0].bn1.register_forward_hook(model.rec_actmaps_hook))
+            #hook_handles.append(model.resnet.layer4[0].bn1.register_forward_hook(model.asm_source_hook))
         
         for batch_idx, batch in enumerate(tqdm(data['train'])):
             
