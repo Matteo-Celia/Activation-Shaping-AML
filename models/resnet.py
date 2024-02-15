8490401c7fec57d714668285d5b48a483e18d6a9
frogeye00 <CLEMTT00E08G371P@studium.unict.it>
2024-02-15 12:09:32 +0100

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -144,7 +144,8 @@ class DGResNet18(nn.Module):
         mask2 = (self.actmaps2.pop(0) > 0).float()
         mask3 = (self.actmaps3.pop(0) > 0).float()
         output_bin = (output > 0).float()
-        mask = torch.cat((mask1, mask2, mask3),0) #should be multiplication
+        m = mask1*mask2*mask3 #should be multiplication
+        mask = torch.cat((m,m,m),0)
         return mask * output_bin
 
 
